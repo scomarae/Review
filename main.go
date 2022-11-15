63c053b19073caddae55e6c7790ac612e6cedac6
scomarae <scomarae@mail.ru>
2022-11-15 16:34:14 +0900

    альбомы

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/gin"
-	_ "log"
-	_ "math/rand"
-	_ "net/http"
-	_ "strconv"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type Book struct {
+// album представляет данные об альбоме.
+type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+// Срез albums для заполнения данных об альбомах.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums возвращает список всех альбомов в формате JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
